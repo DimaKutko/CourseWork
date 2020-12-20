3c133c1022f9aff710807b228007eb74dea0d6d5
DimaKutko <dimakutko410@gmail.com>
2020-12-21 00:29:54 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,36 +3,25 @@
 #include "config.h"
 #include "messages.h"
 
-#include <sys/types.h>
-#include <sys/msg.h>
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-
 pid_t pid1, pid2, pid3, pid4;
-
 
 // D - File1
 void process1(){
-//    printf("PROCESS 1 \n");
     receiveNums(RECEIVER);
 }
 
 // A - File2
 void process2(){
-//    printf("PROCESS 2 \n");
     sendNums(F_SENDER);
 }
 
 // B - Console
 void process3(){
-//    printf("PROCESS 3 \n");
     sendNums(S_SENDER);
 }
 
 // C - File4
 void process4(){
-//    printf("PROCESS 4 \n");
     sendNums(T_SENDER);
 }
 
